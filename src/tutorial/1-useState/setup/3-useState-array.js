a1b735d202e8276177645f25a0758060eb7812cd
Tahidur-rahman <mdtahidurrahman20@gmail.com>
2021-01-06 23:44:26 +0600

    useState array

--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -1,8 +1,30 @@
-import React from 'react';
-import { data } from '../../../data';
+import React, { useState } from "react";
+import { data } from "../../../data";
 
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [state, setState] = useState(data);
+  const removeme = (id)=>{
+    let newstate = state.filter(a=> id !== a.id)
+    setState(newstate);
+    console.log(state)
+  }
+  return (
+    <>
+      {state.map((item) => {
+        let { id, name } = item;
+        return (
+          <>
+            <div className="item"key={id}>
+              {name}
+              <button className="btn" onClick={()=>removeme(id)}>remove</button>
+            </div>
+            
+          </>
+        );
+      })}
+      <button className="btn" onClick={()=>setState([])}>Delete</button>
+    </>
+  );
 };
 
 export default UseStateArray;
